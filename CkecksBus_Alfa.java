084cd7ae64e1dba2502926ca7b4111ed09657a5c
Fabian-Code <f.mariqueo01@ufromail.cl>
2016-09-26 03:13:52 -0300

    Update CkecksBus_Alfa.java
    Se cambiaron cosas varias como:
    - metodo validar hora
    - bug del metodo validar fecha
    - metodo del submenu itinerario

--- CkecksBus_Alfa.java
+++ CkecksBus_Alfa.java
@@ -15,7 +15,8 @@ public class CkecksBus_Alfa {
 
     /**
      * @param args the command line arguments
-     */
+     */    
+    
    public static void main(String[] args) {
          menu();
     }
@@ -36,11 +37,12 @@ public class CkecksBus_Alfa {
             System.out.println("1 - Funcionarios");
             System.out.println("2 - Itinerario");
             System.out.println("3 - Mostrar Terminal");
+            System.out.println("4 - Mostrar itinerario de los buses del terminal");
             System.out.println("0 - Salir");
             do {
                 System.out.println("Ingrese opcion");
                 opcion = leer.next();
-            } while (!"1".equals(opcion) && !"2".equals(opcion) && !"3".equals(opcion) && !"0".equals(opcion));
+            } while (!"1".equals(opcion) && !"2".equals(opcion) && !"3".equals(opcion) && !"4".equals(opcion) && !"0".equals(opcion));
             switch (opcion) {
                 case "1":
                     menuFuncionarios(funcionarios);
@@ -51,6 +53,11 @@ public class CkecksBus_Alfa {
                 case "3":
                     mostrarTerminal(nombreterminal, region, direccion, funcionarios, itinerario);
                     break;
+                    
+                case "4":
+                    mostrarItinerario(itinerario);
+                    break;
+                    
                 case "0":
                     System.out.println("Adios");
                     break;
@@ -62,15 +69,10 @@ public class CkecksBus_Alfa {
 
     public static void mostrarTerminal(String nombreterminal, String region, String direccion, ArrayList<String> funcionarios, ArrayList<String> itinerario) {
        System.out.println("Nombre de Terminal: "+ nombreterminal+" Region: "+region+" Direccion: "+direccion);
-        System.out.println("Funcionarios del Terminal");
-        for (Iterator i = funcionarios.iterator(); i.hasNext();) {
-            System.out.println(i.next());
-        }
-        System.out.println("Itinerario del Terminal;");
-        for (Iterator i = itinerario.iterator(); i.hasNext();) {
-            System.out.println(i.next());
-        }
-
+        System.out.println("Funcionarios del Terminal:");
+        mostrarFuncionarios(funcionarios);
+        System.out.println("Itinerario del Terminal:");
+        mostrarItinerario(itinerario);        
     }
 
     public static void menuFuncionarios(ArrayList<String> funcionarios) {
@@ -142,6 +144,7 @@ public class CkecksBus_Alfa {
     }
     /*Esta que entrega el String que coideciden con los datos que se piden,  dado que los concatenan en un solo String y lo busca en el ArrayList funcionario que tiene
     que tiene misma secuencia de caracteres */
+    
     public static String validarExistenciaFuncionarioTerminal(String nombrefuncionario,String empresa,String nombreusu, ArrayList<String> funcionarios){
     String funcionario1 = "Funcionario: " + nombrefuncionario + "\t Empresa: " + empresa + "\n Nombre de usuario: " + nombreusu + " \t contraseña: *********";
         for (String l : funcionarios) {
@@ -182,29 +185,40 @@ public class CkecksBus_Alfa {
                 opcion=leer.next();
             } while (!(opcion.equals("1") || opcion.equals("2") || opcion.equals("3")));
             
+            eliminarFuncionario(nombreOriginal, empresaOriginal, usuarioOriginal,funcionarios);//Se elimina el funcionario
+            //para volver a agregarlo con los datos actualizados
+            
             switch(opcion){
                 
                 case "1": System.out.println("Nuevo nombre:");
                 leer.nextLine();
-                nombre=leer.nextLine();
-                eliminarFuncionario(nombreOriginal, empresaOriginal, usuarioOriginal,funcionarios);
+                    do {
+                        nombre=leer.nextLine();
+                    } while (!ValidarNombres(nombre));                                                    
                 añadirFuncionarioTerminal(nombre,empresaOriginal, usuarioOriginal,funcionarios);
                 break;
                     
                 case "2": System.out.println("Nueva empresa:");
                 leer.nextLine();
-                empresa=leer.nextLine();
-                eliminarFuncionario(nombreOriginal, empresaOriginal, usuarioOriginal,funcionarios);
+                    do {
+                        empresa=leer.nextLine();
+                    } while (!ValidarNombres(empresa));                                                    
                 añadirFuncionarioTerminal(nombreOriginal,empresa, usuarioOriginal,funcionarios);
                 break;
                 
                 case "3": System.out.println("Nuevo nombre de usuario:");
                 leer.nextLine();
-                usuario=leer.nextLine();
-                eliminarFuncionario(nombreOriginal, empresaOriginal, usuarioOriginal,funcionarios);
+                    do {
+                        usuario=leer.nextLine();
+                    } while (!ValidarNombres(usuario));                                                    
                 añadirFuncionarioTerminal(nombreOriginal,empresaOriginal, usuario,funcionarios);
                 break;
                 
+                default: 
+                    System.out.println("Opcion invalida");
+                    añadirFuncionarioTerminal(nombreOriginal, empresaOriginal, usuarioOriginal,funcionarios);//Si no se hace ningun cambio
+                    //se vuelve a agregar el funcionario con todos los datos originales
+                    break;
             }
         }else{
             System.out.println("El funcionario ingresado no existe");
@@ -215,6 +229,13 @@ public class CkecksBus_Alfa {
     public static void mostrarFuncionarios(ArrayList<String> funcionarios){
         System.out.println("Funcionarios");
          for (Iterator i = funcionarios.iterator(); i.hasNext();) {
+            System.out.println(i.next());
+        }
+    }
+    
+    public static void mostrarItinerario(ArrayList <String> itinerario){
+        System.out.println("Itinerario");
+        for (Iterator i=itinerario.iterator(); i.hasNext();) {
             System.out.println(i.next());
         }
     }
@@ -286,8 +307,43 @@ public class CkecksBus_Alfa {
                         añadirItinerarioBus(estadoBus, destino, empresa, horaSalidaBus, horaLlegadaBus, fecha, itinerario);
                         break;
                     case "2":
+                        System.out.println("Datos del itinerario a eliminar");
+                        leer.nextLine();
+                        
+                        do {
+                            System.out.println("Ingrese estado de bus:(ej: atrasado, llego, etc.)");
+                            estadoBus = leer.nextLine();
+                        } while (!ValidarNombres(estadoBus));
+                        
+                        do {
+                            System.out.println("Ingrese destino de bus");
+                            destino = leer.nextLine();
+                        } while (!ValidarNombres(destino));
+                        
+                        do {
+                            System.out.println("Ingrese empresa que pertenece el bus");
+                            empresa = leer.nextLine();
+                        } while (!ValidarNombres(empresa));
+                        
+                        do {
+                            System.out.println("Ingrese Hora Salida del Bus: (ej: hh:mm)");
+                            horaSalidaBus = leer.next();
+                        } while (!ValidarHora(horaSalidaBus));
+                        
+                        do {
+                            System.out.println("Ingrese Hora LLegada del Bus: (ej: hh:mm) ");
+                            horaLlegadaBus = leer.next();
+                        } while (!ValidarHora(horaLlegadaBus));//el ciclo seguira mientras el formato de hora no se cumpla
+                        
+                         do {
+                            System.out.println("Ingrese Fecha: (ej: dd/mm/yyyy)");
+                            fecha = leer.next();
+                        } while (!ValidarFecha(fecha));
+                        
+                        eliminarItinerario(estadoBus, destino, empresa, horaSalidaBus, horaLlegadaBus, fecha,itinerario);
                         break;
                     case "3":
+                        menuModificarItinerario(itinerario);
                         break;
                     case "0":
                         break;
@@ -297,11 +353,125 @@ public class CkecksBus_Alfa {
             } while (!"0".equals(opc));
         }
     }
+    
+    public static void menuModificarItinerario(ArrayList<String> itinerario){
+        Scanner leer= new Scanner(System.in);
+        String opc;
+        String estado, destino,empresa,horaSalida,horaLlegada,fecha;
+        String  estadoOriginal,destinoOriginal,empresaOriginal,horaSalidaOriginal,horaLlegadaOriginal,fechaOriginal;
+        leer.nextLine();
+        
+        
+        do {
+            System.out.println("Ingrese estado de bus:(ej: atrasado, llego, etc.)");
+            estado = leer.nextLine();
+        } while (!ValidarNombres(estado));
+                    
+        do {
+            System.out.println("Ingrese destino de bus");
+            destino = leer.nextLine();
+        } while (!ValidarNombres(destino));
+                        
+        do {
+            System.out.println("Ingrese empresa que pertenece el bus");
+            empresa = leer.nextLine();
+        } while (!ValidarNombres(empresa));
+                        
+        do {
+            System.out.println("Ingrese Hora Salida del Bus: (ej: hh:mm)");
+            horaSalida = leer.next();
+        } while (!ValidarHora(horaSalida));
+                        
+        do {
+            System.out.println("Ingrese Hora LLegada del Bus: (ej: hh:mm) ");
+            horaLlegada = leer.next();
+        } while (!ValidarHora(horaLlegada));//el ciclo seguira mientras el formato de hora no se cumpla
+                        
+        do {
+            System.out.println("Ingrese Fecha: (ej: dd/mm/yyyy)");
+            fecha = leer.next();
+        } while (!ValidarFecha(fecha));
+                
+        estadoOriginal=estado;        
+        destinoOriginal=destino;
+        empresaOriginal=empresa;
+        horaSalidaOriginal=horaSalida;
+        horaLlegadaOriginal=horaLlegada;
+        fechaOriginal=fecha;
+        
+        if(validarAñadirItinerarioBus(estado, destino,empresa,horaSalida,horaLlegada,fecha,itinerario)!=null){
+            System.out.println("1- Modificar estado del bus"+"\n2 - Modificar destino"+"\n3 - Modificar empresa"+
+                    "\n4 - Modificar hora salida"+"\n5 - Modificar hora llegada"+"\n6 - Modificar fecha");
+            
+            do {
+                System.out.println("Ingrese opcion");
+                opc=leer.nextLine();
+            } while(!(opc.equals("1") || opc.equals("2")  || opc.equals("3")  || opc.equals("4")  || opc.equals("5") || opc.equals("6")));
+            eliminarItinerario(estadoOriginal,destinoOriginal,empresaOriginal,horaSalidaOriginal,horaLlegadaOriginal,fechaOriginal,itinerario);
+            
+            switch(opc){
+            
+                case "1": 
+                    do {
+                        System.out.println("Nuevo estado:");
+                        estado=leer.nextLine();
+                    } while (!ValidarNombres(estado));                                                            
+                    añadirItinerarioBus(estado,destinoOriginal,empresaOriginal,horaSalidaOriginal,horaLlegadaOriginal,fechaOriginal,itinerario);
+                    break; 
+                
+                case "2":
+                    do {
+                        System.out.println("Nuevo destino");
+                        destino=leer.nextLine();  
+                    } while (!ValidarNombres(destino));                                                         
+                    añadirItinerarioBus(estadoOriginal,destino,empresaOriginal,horaSalidaOriginal,horaLlegadaOriginal,fechaOriginal,itinerario);
+                    break;
+                    
+                case "3":
+                    do {
+                        System.out.println("Nueva empresa");
+                        empresa=leer.nextLine(); 
+                    } while (!ValidarNombres(empresa));
+                                       
+                    añadirItinerarioBus(estadoOriginal,destinoOriginal,empresa,horaSalidaOriginal,horaLlegadaOriginal,fechaOriginal,itinerario);
+                    break;
+                    
+                case "4":
+                    do {
+                        System.out.println("Nueva hora de salida");
+                        horaSalida=leer.nextLine();
+                    } while (!ValidarHora(horaSalida));                                        
+                    añadirItinerarioBus(estadoOriginal,destinoOriginal,empresaOriginal,horaSalida,horaLlegadaOriginal,fechaOriginal,itinerario);
+                    break;
+                    
+                case "5":
+                    do {
+                        System.out.println("Nueva hora de llegada");
+                        horaLlegada=leer.nextLine();
+                    } while (!ValidarHora(horaLlegada));                                                       
+                    añadirItinerarioBus(estadoOriginal,destinoOriginal,empresaOriginal,horaSalidaOriginal,horaLlegada,fechaOriginal,itinerario);
+                    break;
+                    
+                case "6":
+                    do {
+                        System.out.println("Nueva fecha");
+                        fecha=leer.nextLine();
+                    } while (!ValidarFecha(fecha));                                                      
+                    añadirItinerarioBus(estadoOriginal,destinoOriginal,empresaOriginal,horaSalidaOriginal,horaLlegadaOriginal,fecha,itinerario);
+                    break;
+                    
+                default: 
+                    System.out.println("Opción invalida");
+                    añadirItinerarioBus(estadoOriginal,destinoOriginal,empresaOriginal,horaSalidaOriginal,horaLlegadaOriginal,fechaOriginal,itinerario);
+                    break;
+            }
+            
+        }
+    }
 
     public static boolean validarUsuario(String usuario, ArrayList<String> funcionario) {
         String val = usuario;
-
-        for (String l : funcionario) {
+        for (String l : funcionario) {            
             if (l.contains(val)) {
                 return true;
             }
@@ -311,27 +481,36 @@ public class CkecksBus_Alfa {
   
     public static void añadirItinerarioBus(String estadoBus, String destino, String empresa, String horaSalidaBus, String horaLlegadaBus, String fecha, ArrayList<String> itinerario) {
 
-        if (validarAñadirItinerarioBus(estadoBus, destino, empresa, horaSalidaBus, horaLlegadaBus, fecha, itinerario)) {
+        if (validarAñadirItinerarioBus(estadoBus, destino, empresa, horaSalidaBus, horaLlegadaBus, fecha, itinerario)==null) {
             String hola = "Fecha: " + fecha + "\n Hora de Llegada: " + horaLlegadaBus + "\t Hora de salida: " + horaSalidaBus
-                    + "\n Empresa: " + empresa + "\t Destino: " + destino + " estado de Bus: " + estadoBus;
+                    + "\n Empresa: " + empresa + "\t Destino: " + destino + "\t estado de Bus: " + estadoBus;
             itinerario.add(hola);
         } else {
             System.out.println("Itinerario de Bus existente");
         }
 
     }
+    
+    public static void eliminarItinerario(String estadoBus, String destino, String empresa, String horaSalidaBus, String horaLlegadaBus, String fecha, ArrayList<String> itinerario){
+        if (validarAñadirItinerarioBus(estadoBus,destino,empresa,horaSalidaBus, horaLlegadaBus,fecha,itinerario)!=null) {
+            itinerario.remove(validarAñadirItinerarioBus(estadoBus,destino,empresa,horaSalidaBus, horaLlegadaBus,fecha,itinerario));
+            System.out.println("Exito al eliminar");
+        }else{System.out.println("El itinerario no existe");}
+    
+    }
 /*El validarAñadirItinerarioBus recive los datos de un itinerario y el ArrayList de String funcionario y genera un String (que en este caso le pusimos iti) que
   contiene la misma secuencia caracteres entre comillas con el que esta en el metodo añadirItinerarioBus para que si el ArrayList contiene algo igual retorne false (Para el metodo que añade diga que existe funcionario) y sino retorne true*/  
-    public static boolean validarAñadirItinerarioBus(String estadoBus, String destino, String empresa, String horaSalidaBus, String horaLlegadaBus, String fecha, ArrayList<String> itinerario) {
+    public static String validarAñadirItinerarioBus(String estadoBus, String destino, String empresa, String horaSalidaBus, String horaLlegadaBus, String fecha, ArrayList<String> itinerario) {
         String iti = "Fecha: " + fecha + "\n Hora de Llegada: " + horaLlegadaBus + "\t Hora de salida: " + horaSalidaBus
                 + "\n Empresa: " + empresa + "\t Destino: " + destino + " estado de Bus: " + estadoBus;
         for (String l : itinerario) {
             if (l.contains(iti)) {
-                return false;
+                return l;
             }
         }
-        return true;
+        return null;
     }
+    
 
     public static boolean ValidarNombres(String nombre) {
         Pattern pat = Pattern.compile("[A-Za-z]+( [A-Za-z]+)*");
@@ -339,20 +518,24 @@ public class CkecksBus_Alfa {
         return (mac.matches());
     }
 
-   public static boolean ValidarFecha(String fecha) {
+    public static boolean ValidarFecha(String fecha) {
         Pattern pat = Pattern.compile("[\\d]{2,2}/[\\d]{2,2}/[\\d]{4,4}");
         Matcher mac = pat.matcher(fecha);
-        SimpleDateFormat df1 = new SimpleDateFormat("dd/MM/yyyy");
-        Date testDate = null;
-        String date1 = fecha;
-        try{
-            testDate = df1.parse(date1);
-        } catch (Exception e){ System.out.println("Formato invalido");}
-        return df1.format(testDate).equals(date1) && (mac.matches());
+        if (mac.matches()) {
+            SimpleDateFormat df1 = new SimpleDateFormat("dd/MM/yyyy");
+            Date testDate = null;
+            String date1 = fecha;
+            try{
+                testDate = df1.parse(date1);
+            } catch (Exception e){ System.out.println("Formato invalido");}
+            return df1.format(testDate).equals(date1) && (mac.matches());
+        }else{
+            return false;
+        }
     }
 
     public static boolean ValidarHora(String hora) {
-        Pattern pat = Pattern.compile("[0-2]*[0-9]{1}:[0-5]{1}[0-9]{1}");
+        Pattern pat = Pattern.compile("(([01]\\d)|(2[0-3])):([0-5]\\d)");
         Matcher mac = pat.matcher(hora);
         return (mac.matches());
     }
